14d06c81fe73cb826164b0dd6e9297c3d0022689
LycrasEuoi <y.claes@outlook.com>
2024-11-13 22:39:57 +0100

    Adding a Function that print the list, if 2 or more files are added on the same date bundle them with a \n char

--- Scripts/AutomateDownloadFolder.py
+++ Scripts/AutomateDownloadFolder.py
@@ -1,10 +1,25 @@
 import os
 import time
+import datetime as dt
 
 def fileNameLimiter(file):
     if len(file) > 60:
         return file[:60]
     return file
+
+def printDownloadList(downloadList):
+
+    downloadList.sort(key=lambda x: x[1])       # Sort list by Date, the second value in the tuple
+
+    print(f'{"Naam":<60}{"Date":>60}')
+    print('-' * 120)                            # Print the header of the list      
+
+    for item in downloadList:
+        if item[1] != downloadList[downloadList.index(item)-1][1]: # Bundels files of same creation date
+            print('\n')
+            print(f'{downloadList.index(item)} {item[0]:<60}{item[1]:>60}')
+        else:
+            print(f'{downloadList.index(item)} {item[0]:<60}{item[1]:>60}')
 
 downloadFolderPath = r'C:\Users\YClae\Downloads'
 
@@ -12,16 +27,11 @@ downloadFiles = os.listdir(downloadFolderPath)
 downloadList = []
 
 for file in downloadFiles:
-    
     filePath = os.path.join(downloadFolderPath, file)
-    timeStampCreationDate = time.ctime(os.path.getctime(filePath))
+    tCreationDate = dt.datetime.fromtimestamp(os.path.getctime(filePath))
+    
     shortendFile = fileNameLimiter(file)
-    downloadList.append((shortendFile, timeStampCreationDate))
+    downloadList.append((shortendFile, tCreationDate.strftime("%a %b %d %Y")))
 
-downloadList.sort(key=lambda x: x[1])
+printDownloadList(downloadList)
 
-print(f'{"Naam":<60}{"Date":>60}')
-print('-' * 120)
-
-for item in downloadList:
-    print(f'{item[0]:<60}{item[1]:>60}')
