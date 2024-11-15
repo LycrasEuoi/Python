fd6050d2ea156cf4a3902eb11f73d20398ef8251
LycrasEuoi <y.claes@outlook.com>
2024-11-15 15:00:52 +0100

    Adding comments and cleaning up the code

--- Scripts/AutomateDownloadFolder.py
+++ Scripts/AutomateDownloadFolder.py
@@ -1,39 +1,42 @@
 #! python3
 
 import os
-import time
 import datetime as dt
 
+
+# A function that limits the charsize of a file, foldername is < 60 char's
 def fileNameLimiter(file):
     if len(file) > 60:
         return file[:60]
     return file
 
+# A function that print a formated list of the conents of the specified folder
 def printDownloadList(downloadList):
 
-    downloadList.sort(key=lambda x: x[1])       # Sort list by Date, the second value in the tuple
+    downloadList.sort(key=lambda x: x[1]) # Sort list by Date, the second value in the tuple
 
-    print(f'{"Naam":<60}{"Date":>60}')
-    print('-' * 120)                            # Print the header of the list      
+    print(f'{"Naam":<60}{"Date":>60}') # Print the header of the list     
+    print('-' * 120)                             
 
-    for item in downloadList:
-        if item[1] != downloadList[downloadList.index(item)-1][1]: # Bundels files of same creation date
-            print('\n')
-            print(f'{downloadList.index(item)} {item[0]:<60}{item[1]:>60}')
-        else:
-            print(f'{downloadList.index(item)} {item[0]:<60}{item[1]:>60}')
+    for item in downloadList: # A for-loop that print every item of a folder, is separatedby a newline between every section with the same date(rounded by day)
+        if item[1] != downloadList[downloadList.index(item)-1][1]:
+            print('\n') # Prints a newline for every bundel that's created on the same date
+        
+        print(f'[{downloadList.index(item)}] {item[0]:<60}{item[1]:>60}')
+            
 
-downloadFolderPath = r'C:\Users\YClae\Downloads'
+downloadFolderPath = r'C:\Users\YClae\Downloads' # Folder of which contents are printed
 
 downloadFiles = os.listdir(downloadFolderPath)
 downloadList = []
 
-for file in downloadFiles:
-    filePath = os.path.join(downloadFolderPath, file)
-    tCreationDate = dt.datetime.fromtimestamp(os.path.getctime(filePath))
-    
-    shortendFile = fileNameLimiter(file)
-    downloadList.append((shortendFile, tCreationDate.strftime("%a %b %d %Y")))
+# A for loop that creates a list-variable of all the items in a folder
+for file in downloadFiles: 
+    filePath = os.path.join(downloadFolderPath, file) # Gets folder name
+    shortendFile = fileNameLimiter(file) 
+
+    tCreationDate = dt.datetime.fromtimestamp(os.path.getctime(filePath)) # gets creation date
+    downloadList.append((shortendFile, tCreationDate.strftime("%a %b %d %Y"))) # Formats creation date
 
 printDownloadList(downloadList)
 
